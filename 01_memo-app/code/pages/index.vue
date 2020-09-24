374bc10288eeabd7764f191b297461ea832098c7
satoshi ishizaki <e1433104@yahoo.co.jp>
2020-09-24 09:08:07 +0900

    01：Nuxtの初期設定

---
+++ 01_memo-app/code/pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+    <button class="button">test</button>
+    <p class="test"><span>aaa</span></p>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss">
+.test {
+  span {
+    color: red;
+  }
+}
+</style>
