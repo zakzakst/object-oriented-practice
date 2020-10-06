61e8e40634dc1b854a78781ca7d63e51b0759ad0
satoshi ishizaki <e1433104@yahoo.co.jp>
2020-10-06 09:49:31 +0900

    練習5：Nuxtの初期設定

---
+++ 05_family-park-app/code/nuxt.config.js
@@ -0,0 +1,69 @@
+export default {
+  // Disable server-side rendering (https://go.nuxtjs.dev/ssr-mode)
+  ssr: false,
+
+  // Target (https://go.nuxtjs.dev/config-target)
+  target: 'static',
+
+  // Global page headers (https://go.nuxtjs.dev/config-head)
+  head: {
+    title: '05_family-park-app',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  // Global CSS (https://go.nuxtjs.dev/config-css)
+  css: [
+    '@/assets/sass/_normalize.scss',
+    '@/assets/sass/_bulma-customize.scss',
+    '@/assets/sass/_common.scss',
+  ],
+
+  // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
+  plugins: [
+  ],
+
+  // Auto import components (https://go.nuxtjs.dev/config-components)
+  components: true,
+
+  // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
+  buildModules: [
+  ],
+
+  // Modules (https://go.nuxtjs.dev/config-modules)
+  modules: [
+    '@nuxtjs/axios',
+    '@nuxtjs/dotenv',
+    '@nuxtjs/style-resources',
+    'nuxt-fontawesome',
+  ],
+
+  // Axios module configuration (https://go.nuxtjs.dev/config-axios)
+  axios: {},
+
+  // Build Configuration (https://go.nuxtjs.dev/config-build)
+  build: {
+  },
+
+  styleResources: {
+    scss: [
+      '@/assets/sass/_variables.scss'
+    ]
+  },
+
+  fontawesome: {
+    component: 'fa',
+    imports: [
+      {
+        set: '@fortawesome/free-solid-svg-icons',
+        icons: ['fas']
+      },
+    ]
+  },
+}
---
+++ 05_family-park-app/code/pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+    <button class="button">test</button>
+    <p class="test"><span>aaa</span></p>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss">
+// # TODO: TODOコメントサンプル
+// # NOTE: NOTEコメントサンプル
+.test {
+  span {
+    color: red;
+  }
+}
+</style>
